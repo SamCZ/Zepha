3d5b705031834aad82f557d51859ee6089db5e74
Nicole Collings <100Toby1@gmail.com>
2019-11-02 17:01:11 -0700

    Double World Gen Queue size

--- src/server/world/WorldGenStream.h
+++ src/server/world/WorldGenStream.h
@@ -17,7 +17,7 @@
 
 class WorldGenStream {
 public:
-    static const int THREAD_QUEUE_SIZE = 32;
+    static const int THREAD_QUEUE_SIZE = 64;
     static const int THREADS = 4;
     static const int TOTAL_QUEUE_SIZE = THREADS * THREAD_QUEUE_SIZE;
 
