ab97b925697e950915912e82a460cd62e2a90d7f
Nicole Collings <100Toby1@gmail.com>
2019-11-12 19:18:11 -0800

    3D Voronoi Diagram implementation~

---
+++ src/util/Voronoi3D.cpp
@@ -0,0 +1,68 @@
+//
+// Created by aurailus on 2019-11-12.
+//
+
+#include "Voronoi3D.h"
+
+Voronoi3D::Voronoi3D(unsigned short size) :
+    size(size) {
+
+    data = voronoi_data(size);
+
+    for (unsigned short i = 0; i < size; i++) {
+        data[i].resize(size);
+        for (unsigned short j = 0; j < size; j++) {
+            data[i][j].resize(size);
+        }
+    }
+}
+
+void Voronoi3D::setPoints(const std::vector<glm::vec3>& points) {
+    this->points = points;
+
+    for (unsigned short i = 0; i < size; i++) {
+        for (unsigned short j = 0; j < size; j++) {
+            for (unsigned short k = 0; k < size; k++) {
+
+                float pointDistance = INFINITY;
+                unsigned short pIndex = 0;
+
+                for (unsigned short p = 0; p < points.size(); p++) {
+                    float thisPointDistance = glm::distance(points[p], {i, j, k});
+                    if (thisPointDistance < pointDistance) {
+                        pointDistance = thisPointDistance;
+                        pIndex = p;
+                    }
+                }
+
+                data[i][j][k] = pIndex;
+            }
+        }
+    }
+}
+
+void Voronoi3D::setColorValues(const std::vector<glm::vec3>& values) {
+    colorValues = values;
+}
+
+void Voronoi3D::generateImage(unsigned short depth) {
+    auto colorData = new unsigned char[size * size * 3];
+
+    depth = depth;
+
+    for (int i = 0; i < size * size; i++) {
+        int x = i % size;
+        int y = i / size;
+
+        unsigned short point = data[x][y][depth];
+        auto color = colorValues[point];
+
+        colorData[i * 3 + 0] = static_cast<int>(color.x * 255.f);
+        colorData[i * 3 + 1] = static_cast<int>(color.y * 255.f);
+        colorData[i * 3 + 2] = static_cast<int>(color.z * 255.f);
+    }
+
+    std::string name = "voronoi_";
+    name += std::to_string(depth) + ".jpg";
+    stbi_write_jpg(name.data(), size, size, 3, colorData, 100);
+}
---
+++ src/util/Voronoi3D.h
@@ -0,0 +1,29 @@
+//
+// Created by aurailus on 2019-11-12.
+//
+
+#pragma once
+
+#include <vector>
+#include <glm/vec4.hpp>
+#include <glm/geometric.hpp>
+
+#include "stb_image/stb_image_write.h"
+
+class Voronoi3D {
+public:
+    Voronoi3D() = default;
+    Voronoi3D(unsigned short size);
+
+    void setPoints(const std::vector<glm::vec3>& points);
+
+    void setColorValues(const std::vector<glm::vec3>& values);
+    void generateImage(unsigned short depth);
+private:
+    typedef std::vector<std::vector<std::vector<unsigned short>>> voronoi_data;
+
+    unsigned short size;
+    voronoi_data data;
+    std::vector<glm::vec3> points;
+    std::vector<glm::vec3> colorValues;
+};
