fb27da49cb36c65f0f40dfb574000585ecfd81a4
Auri <me@auri.xyz>
2020-11-08 18:14:51 -0800

    Remove unused CreateBlockModel class.

--- src/lua/register/CreateBlockModel.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by aurailus on 2020-07-19.
-//
-
-#include "CreateBlockModel.h"
--- src/lua/register/CreateBlockModel.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by aurailus on 2020-07-19.
-//
-
-#pragma once
-
-class CreateBlockModel {
-
-};
